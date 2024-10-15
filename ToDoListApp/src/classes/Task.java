b4142642c1f49450c647f18b2b4569b66b91a748
bayronjoshua333 <joshua.bayron@awsys-i.com>
2024-10-15 15:31:41 +0800

    Add priority feature to Task class

--- ToDoListApp/src/classes/Task.java
+++ ToDoListApp/src/classes/Task.java
@@ -1,16 +1,25 @@
 package classes;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class Task {
     private static int idCounter = 1;
     private int id;
     private String description;
     private String deadline;
     private boolean isCompleted;
-    public Task(String description, String deadline) {
+    private String priority;
+    
+    public Task(String description, String deadline, String priority) {
         this.id = idCounter++;
         this.description = description;
         this.deadline = deadline;
         this.isCompleted = false;
+        this.priority = priority;
     }
+    
     public int getId() {
         return id;
     }
@@ -23,6 +32,11 @@ public class Task {
     public boolean isCompleted() {
         return isCompleted;
     }
+    
+    public String getPriority() {
+    	return priority;
+    }
+    
     public void markAsCompleted() {
         this.isCompleted = true;
     }
